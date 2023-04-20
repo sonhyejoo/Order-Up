77fcb12001cc62006ca1c4dc2dd0d67c953f4c9f
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-20 18:19:11 -0400

    Add working login/logout

--- app/config.py
+++ app/config.py
@@ -3,3 +3,8 @@ import os
 
 class Configuration:
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DATABASE_URL")
+        # or "postgresql://oder_up:9uCxydbt@localhost/order_up_dv"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

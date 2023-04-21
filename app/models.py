5a9665d952302f051faf0a806b1749cb8daa707a
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-21 13:26:44 -0400

    Add table, order, order_detail definitions

--- app/models.py
+++ app/models.py
@@ -6,10 +6,13 @@ db = SQLAlchemy()
 
 
 class Employee(db.Model, UserMixin):
+    __tablename__ = "employees"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     employee_number = db.Column(db.Integer, nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    orders = db.relationship("Order", back_populates="employee")
 
     @property
     def password(self):
@@ -43,6 +46,7 @@ class MenuItem(db.Model):
     )
     menu = db.relationship("Menu", back_populates="items")
     type = db.relationship("MenuItemType", back_populates="items")
+    details = db.relationship("OrderDetail", back_populates="item")
 
 
 class MenuItemType(db.Model):
@@ -51,3 +55,46 @@ class MenuItemType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     items = db.relationship("MenuItem", back_populates="type")
+
+
+class Table(db.Model):
+    __tablename__ = "tables"
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False, unique=True)
+    capacity = db.Column(db.Integer, nullable=False)
+    orders = db.relationship("Order", back_populates="table")
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    table_id = db.Column(
+        db.Integer,
+        db.ForeignKey("tables.id"),
+        nullable=False,
+    )
+    server_id = db.Column(
+        db.Integer,
+        db.ForeignKey("employees.id"),
+        nullable=False,
+    )
+    finished = db.Column(db.Boolean, nullable=False)
+    employee = db.relationship("Employee", back_populates="orders")
+    table = db.relationship("Table", back_populates="orders")
+    details = db.relationship("OrderDetail", back_populates="order")
+
+
+class OrderDetail(db.Model):
+    __tablename__ = "order_details"
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(
+        db.Integer,
+        db.ForeignKey("orders.id"),
+        nullable=False,
+    )
+    menu_item_id = db.Column(db.Integer, db.ForeignKey("menu_items.id"))
+    order = db.relationship("Order", back_populates="details")
+    item = db.relationship("MenuItem", back_populates="details")
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app import app, db
-from app.models import Employee, Menu, MenuItem, MenuItemType
+from app.models import Employee, Menu, MenuItem, MenuItemType, Table
 
 with app.app_context():
     db.drop_all()
@@ -19,6 +19,12 @@ with app.app_context():
     fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
     drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
     jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+
+    tables = list()
+    for i in range(10):
+        tables.append(Table(id=i, number=i, capacity=i + 2))
+
     db.session.add(employee)
     db.session.add(dinner)
+    db.session.add_all(tables)
     db.session.commit()

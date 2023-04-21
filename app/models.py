d1b267eba46da1ed08b3330ed8ee856bda921a80
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-21 11:37:33 -0400

    Add and seed Menu, MenuItem, MenuItemType

--- app/models.py
+++ app/models.py
@@ -21,3 +21,33 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Menu(db.Model):
+    __tablename__ = "menus"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    items = db.relationship("MenuItem", back_populates="menu")
+
+
+class MenuItem(db.Model):
+    __tablename__ = "menu_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
+    menu_type_id = db.Column(
+        db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False
+    )
+    menu = db.relationship("Menu", back_populates="items")
+    type = db.relationship("MenuItemType", back_populates="items")
+
+
+class MenuItemType(db.Model):
+    __tablename__ = "menu_item_types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    items = db.relationship("MenuItem", back_populates="type")
--- database.py
+++ database.py
@@ -3,12 +3,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app import app, db
-from app.models import Employee
+from app.models import Employee, Menu, MenuItem, MenuItemType
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
     employee = Employee(name="Margo", employee_number=1234, password="password")
+    beverages = MenuItemType(name="Beverages")
+    entrees = MenuItemType(name="Entrees")
+    sides = MenuItemType(name="Sides")
+
+    dinner = Menu(name="Dinner")
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
     db.session.add(employee)
+    db.session.add(dinner)
     db.session.commit()

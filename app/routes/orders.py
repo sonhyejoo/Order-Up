a9c76632cc3e4b4a9bd632aa6afe8cbdd966564e
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-21 14:00:21 -0400

    Add AssignTable form route, still requires post

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from ..forms import AssignTableForm
+from ..models import Employee, Table, Order
 
 bp = Blueprint("orders", __name__, url_prefix="")
 
@@ -7,4 +9,31 @@ bp = Blueprint("orders", __name__, url_prefix="")
 @bp.route("/")
 @login_required
 def index():
-    return render_template("orders.html")
+    form = AssignTableForm()
+
+    # Assign Table form queries
+    tables = Table.query.order_by(Table.number).all()
+    servers = Employee.query.order_by(Employee.employee_number).all()
+    open_orders = Order.query.filter(Order.finished is False)
+
+    busy_table_ids = [order.table_id for order in open_orders]
+
+    open_tables = [table for table in tables if table.id not in busy_table_ids]
+
+    form.tables.choices = [(t.id, f"Table {t.number}") for t in open_tables]
+    form.servers.choices = [(s.employee_number, f"{s.name}") for s in servers]
+
+    return render_template("orders.html", form=form)
+
+
+@bp.route("/assign", methods=["POST"])
+def assign():
+    form = AssignTableForm()
+    tables = Table.query.order_by(Table.number).all()
+    open_orders = Order.query.filter(Order.finished is False)
+
+    busy_table_ids = [order.table_id for order in open_orders]
+
+    open_tables = [table for table in tables if table.id not in busy_table_ids]
+
+    form.tables.choices = [(t.id, f"Table {t.number}") for t in open_tables]
--- database.py
+++ database.py
@@ -3,13 +3,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app import app, db
-from app.models import Employee, Menu, MenuItem, MenuItemType, Table
+from app.models import (
+    Employee,
+    Menu,
+    MenuItem,
+    MenuItemType,
+    Table,
+    Order,
+    OrderDetail,
+)
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
-    employee = Employee(name="Margo", employee_number=1234, password="password")
+    employees = [
+        Employee(name="Margo", employee_number=1234, password="password"),
+        Employee(name="Mal", employee_number=1235, password="password1"),
+    ]
     beverages = MenuItemType(name="Beverages")
     entrees = MenuItemType(name="Entrees")
     sides = MenuItemType(name="Sides")
@@ -18,13 +29,26 @@ with app.app_context():
 
     fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
     drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
-    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+    jambalaya = MenuItem(
+        name="Jambalaya",
+        price=21.98,
+        type=entrees,
+        menu=dinner,
+    )
 
     tables = list()
     for i in range(10):
         tables.append(Table(id=i, number=i, capacity=i + 2))
+    orders = list()
+    orderDetails = list()
+    for i in range(3):
+        orders.append(Order(id=i, table_id=i, server_id=1, finished=False))
+        orderDetails.append(OrderDetail(id=i, order_id=i, menu_item_id=i + 1))
 
-    db.session.add(employee)
+    db.session.add_all(employees)
     db.session.add(dinner)
     db.session.add_all(tables)
+    db.session.add_all(orders)
+    db.session.add_all(orderDetails)
+
     db.session.commit()
